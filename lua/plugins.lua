cf8831467a2c7802630f6427563920bae3ed282f
zeauw <me@zeauw.dev>
2024-11-25 21:41:11 +0800

    feat: remove wakatime && update plugins

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -98,13 +98,16 @@ require("lazy").setup({
 		highlight = { enable = true },
         })
 	end},
-
-	{"iamcco/markdown-preview.nvim",
-	cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-	ft = { "markdown" },
-	build = function() vim.fn["mkdp#util#install"]() end,},
-
-	{"wakatime/vim-wakatime", lazy = false},
+        
+	{
+		"iamcco/markdown-preview.nvim",
+		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+		build = "cd app && npm install",
+		init = function()
+			vim.g.mkdp_filetypes = { "markdown" }
+		end,
+		ft = { "markdown" },
+	},
 
 	{'Vigemus/iron.nvim'},
 
