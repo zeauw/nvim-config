e5a74d720eac5651b3fca42a6996725a4a9bb5ff
zeauw <me@zeauw.dev>
2025-03-12 20:41:24 +0800

    feat: remove remote support

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -131,17 +131,6 @@ require("lazy").setup({
     },
 
     {
-    "amitds1997/remote-nvim.nvim",
-    version = "*", -- Pin to GitHub releases
-    dependencies = {
-        "nvim-lua/plenary.nvim", -- For standard functions
-        "MunifTanjim/nui.nvim", -- To build the plugin UI
-        "nvim-telescope/telescope.nvim", -- For picking b/w different remote methods
-        },
-    config = true,
-    },
-
-    {
     "folke/which-key.nvim",
     event = "VeryLazy",
     opts = {
