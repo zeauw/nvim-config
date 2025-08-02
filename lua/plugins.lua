8f21aba1ca6486702c773d13f66548b52e183271
zeauw <me@zeauw.dev>
2025-08-02 21:34:39 +0800

    feat: add integration for neogit

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -63,9 +63,7 @@ require("lazy").setup({
         dependencies = {
             "nvim-lua/plenary.nvim", -- required
             "sindrets/diffview.nvim", -- optional - Diff integration
-            -- Only one of these is needed, not both.
             "nvim-telescope/telescope.nvim" -- optional
-            -- "ibhagwan/fzf-lua",              -- optional
         },
         config = true
     }, {
