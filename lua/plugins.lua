27edd31990391a0cb0a868fd49fb7ac23e3070a5
zeauw <me@zeauw.dev>
2025-10-11 14:45:12 +0800

    feat: add image support with kitty

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -236,6 +236,15 @@ require("lazy").setup({
     },
 
     {
+        "3rd/image.nvim",
+        build = false, -- so that it doesn't build the rock https://github.com/3rd/image.nvim/issues/91#issuecomment-2453430239
+        opts = {
+            backend = "kitty",
+            processor = "magick_cli",
+        }
+    },
+
+    {
         "zk-org/zk-nvim",
         config = function()
             require("zk").setup({
