de979865f32ca69bdb97dd360f266e0e5571e049
zeauw <me@zeauw.dev>
2025-10-14 22:15:19 +0800

    chore: use sixel

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -239,7 +239,7 @@ require("lazy").setup({
         "3rd/image.nvim",
         build = false, -- so that it doesn't build the rock https://github.com/3rd/image.nvim/issues/91#issuecomment-2453430239
         opts = {
-            backend = "kitty",
+            backend = "sixel",
             processor = "magick_cli",
         }
     },
